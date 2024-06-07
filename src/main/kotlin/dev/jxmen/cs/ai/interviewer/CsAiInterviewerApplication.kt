3065af22513ca12c0a99fa8aad945c7b6a674e4b
jxmen <sprnd645@gmail.com>
2024-06-07 22:32:39 +0900

    feat: 첫 화면 thymeleaf 파일로 교체
    head 태그는 layout/header.html 파일로 분리

--- src/main/kotlin/dev/jxmen/cs/ai/interviewer/CsAiInterviewerApplication.kt
+++ src/main/kotlin/dev/jxmen/cs/ai/interviewer/CsAiInterviewerApplication.kt
@@ -2,20 +2,20 @@ package dev.jxmen.cs.ai.interviewer
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-open class CsAiInterviewerApplication
+class CsAiInterviewerApplication
 
 fun main(args: Array<String>) {
     runApplication<CsAiInterviewerApplication>(*args)
 }
 
-@RestController
+@Controller
 class IndexController {
     @GetMapping("/")
-    fun hello(): String {
-        return "Hello, World!"
+    fun index(): String {
+        return "index"
     }
 }
