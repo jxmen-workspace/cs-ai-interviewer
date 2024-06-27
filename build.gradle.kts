de84e92edb871ed204828e6e88dff43cd174892f
jxmen <sprnd645@gmail.com>
2024-06-27 15:18:01 +0900

    build: flyway 의존성 추가 - core, mysql

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // database
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-mysql")
     implementation("com.h2database:h2")
 
     /**
