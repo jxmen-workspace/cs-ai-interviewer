bafb76d028ce182ab6701935cf79b9a7302c9ed6
jxmen <sprnd645@gmail.com>
2024-06-27 20:58:22 +0900

    fix: 절대경로로 되어있는 openapi file 경로 수정
    lcoalhost:8080 경로를 사용하지 않고 상대경로를 사용하도록 변경

--- src/main/resources/static/swagger-ui/swagger-initializer.js
+++ src/main/resources/static/swagger-ui/swagger-initializer.js
@@ -1,7 +1,7 @@
 /**
  * NOTE(2024.06.27): 기본값에서 서버에서 제공하는 파일 URL로 변경하였다.
  */
-const OPENAPI_FILE_URL = "http://localhost:8080/swagger-ui/openapi3.yaml";
+const OPENAPI_FILE_URL = "/swagger-ui/openapi3.yaml";
 
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
