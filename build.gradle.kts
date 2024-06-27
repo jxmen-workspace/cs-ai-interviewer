1b89176ed0629f23ea6a1cebb5cca1a6d7eb8728
jxmen <sprnd645@gmail.com>
2024-06-27 17:15:16 +0900

    build: bump up to 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "dev.jxmen"
-version = "0.0.1-SNAPSHOT"
+version = "0.2.0-SNAPSHOT" // NOTE: snapshot은 개발 중인 버전을 의미
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
