66b5db780c1c7791b3126ed668f134a7fc93d71a
jxmen <sprnd645@gmail.com>
2024-06-29 00:10:58 +0900

    feat: local/default 프로파일에서 주제 데이터 추가
    - saveAll 메서드 사용을 위해 JpaRepository 삭제, 기존 인터페이스에서 repository 상속
    - 현재는 DSA 카테고리만 추가된 상태이며, 추후 다른 카테고리 데이터도 추가 예정

--- src/test/kotlin/dev/jxmen/cs/ai/interviewer/domain/subject/service/SubjectServiceTest.kt
+++ src/test/kotlin/dev/jxmen/cs/ai/interviewer/domain/subject/service/SubjectServiceTest.kt
@@ -6,6 +6,13 @@ import dev.jxmen.cs.ai.interviewer.domain.subject.SubjectRepository
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import org.springframework.data.domain.Example
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.repository.query.FluentQuery
+import java.util.Optional
+import java.util.function.Function
 
 class SubjectServiceTest :
     DescribeSpec({
@@ -53,4 +60,73 @@ class StubSubjectRepository : SubjectRepository {
                 )
             SubjectCategory.OS -> listOf(Subject(title = "OS", question = "What is OS?", category = SubjectCategory.OS))
         }
+
+    override fun <S : Subject?> save(entity: S & Any): S & Any = TODO("Not yet implemented")
+
+    override fun <S : Subject?> saveAll(entities: MutableIterable<S>): MutableList<S> = TODO("Not yet implemented")
+
+    override fun <S : Subject?> findAll(example: Example<S>): MutableList<S> = TODO("Not yet implemented")
+
+    override fun <S : Subject?> findAll(
+        example: Example<S>,
+        sort: Sort,
+    ): MutableList<S> = TODO("Not yet implemented")
+
+    override fun findAll(): MutableList<Subject> = TODO("Not yet implemented")
+
+    override fun findAll(sort: Sort): MutableList<Subject> = TODO("Not yet implemented")
+
+    override fun findAll(pageable: Pageable): Page<Subject> = TODO("Not yet implemented")
+
+    override fun <S : Subject?> findAll(
+        example: Example<S>,
+        pageable: Pageable,
+    ): Page<S> = TODO("Not yet implemented")
+
+    override fun findAllById(ids: MutableIterable<Long>): MutableList<Subject> = TODO("Not yet implemented")
+
+    override fun count(): Long = TODO("Not yet implemented")
+
+    override fun <S : Subject?> count(example: Example<S>): Long = TODO("Not yet implemented")
+
+    override fun delete(entity: Subject) = TODO("Not yet implemented")
+
+    override fun deleteAllById(ids: MutableIterable<Long>) = TODO("Not yet implemented")
+
+    override fun deleteAll(entities: MutableIterable<Subject>) = TODO("Not yet implemented")
+
+    override fun deleteAll() = TODO("Not yet implemented")
+
+    override fun <S : Subject?> findOne(example: Example<S>): Optional<S> = TODO("Not yet implemented")
+
+    override fun <S : Subject?> exists(example: Example<S>): Boolean = TODO("Not yet implemented")
+
+    override fun <S : Subject?, R : Any?> findBy(
+        example: Example<S>,
+        queryFunction: Function<FluentQuery.FetchableFluentQuery<S>, R>,
+    ): R & Any = TODO("Not yet implemented")
+
+    override fun flush() = TODO("Not yet implemented")
+
+    override fun <S : Subject?> saveAndFlush(entity: S & Any): S & Any = TODO("Not yet implemented")
+
+    override fun <S : Subject?> saveAllAndFlush(entities: MutableIterable<S>): MutableList<S> = TODO("Not yet implemented")
+
+    override fun deleteAllInBatch(entities: MutableIterable<Subject>) = TODO("Not yet implemented")
+
+    override fun deleteAllInBatch() = TODO("Not yet implemented")
+
+    override fun deleteAllByIdInBatch(ids: MutableIterable<Long>) = TODO("Not yet implemented")
+
+    override fun getReferenceById(id: Long): Subject = TODO("Not yet implemented")
+
+    override fun getById(id: Long): Subject = TODO("Not yet implemented")
+
+    override fun getOne(id: Long): Subject = TODO("Not yet implemented")
+
+    override fun deleteById(id: Long) = TODO("Not yet implemented")
+
+    override fun existsById(id: Long): Boolean = TODO("Not yet implemented")
+
+    override fun findById(id: Long): Optional<Subject> = TODO("Not yet implemented")
 }
