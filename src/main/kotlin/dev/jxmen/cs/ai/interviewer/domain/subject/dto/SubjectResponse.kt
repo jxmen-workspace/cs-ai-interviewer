18d72f46271b47b05295bcc1abd1505588caa766
jxmen <sprnd645@gmail.com>
2024-06-29 23:36:08 +0900

    fix: 누락된 주제 조회 API id 식별자 추가
    Close #18

--- src/main/kotlin/dev/jxmen/cs/ai/interviewer/domain/subject/dto/SubjectResponse.kt
+++ src/main/kotlin/dev/jxmen/cs/ai/interviewer/domain/subject/dto/SubjectResponse.kt
@@ -3,6 +3,7 @@ package dev.jxmen.cs.ai.interviewer.domain.subject.dto
 import dev.jxmen.cs.ai.interviewer.domain.subject.SubjectCategory
 
 data class SubjectResponse(
+    val id: Long, // String?
     val title: String,
     val question: String,
     val category: SubjectCategory,
