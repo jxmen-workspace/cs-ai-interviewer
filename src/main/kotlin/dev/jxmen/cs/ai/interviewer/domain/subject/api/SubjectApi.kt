0f11f6cf28e874950ae8257b87e557047ee94d7f
jxmen <sprnd645@gmail.com>
2024-06-30 00:15:24 +0900

    feat: 주제 목록 조회 API에서 question 필드 제거
    - over fetching이라고 판단되어 추후에 필요하면 추가할 예정
    - repository에서 조회 시에는 subject 컬럼 모두 조회하고 있어 최적화 필요
    
    Close #20

--- src/main/kotlin/dev/jxmen/cs/ai/interviewer/domain/subject/api/SubjectApi.kt
+++ src/main/kotlin/dev/jxmen/cs/ai/interviewer/domain/subject/api/SubjectApi.kt
@@ -24,7 +24,6 @@ class SubjectApi(
                     SubjectResponse(
                         id = it.id,
                         title = it.title,
-                        question = it.question,
                         category = it.category,
                     )
                 },
