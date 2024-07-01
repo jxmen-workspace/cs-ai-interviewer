21009dd0cedc65090d85636320b1f734ecad7a75
jxmen <sprnd645@gmail.com>
2024-07-02 02:28:44 +0900

    feat: dev profile 및 docker-compose 파일 추가

--- src/main/kotlin/dev/jxmen/cs/ai/interviewer/domain/BaseEntity.kt
+++ src/main/kotlin/dev/jxmen/cs/ai/interviewer/domain/BaseEntity.kt
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
+import org.hibernate.annotations.Comment
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import java.time.LocalDateTime
@@ -17,9 +18,11 @@ abstract class BaseEntity {
 
     @Column(nullable = false, updatable = false)
     @CreatedDate
+    @Comment("생성일")
     var createdAt: LocalDateTime = LocalDateTime.now()
 
     @Column(nullable = false)
     @LastModifiedDate
+    @Comment("수정일")
     var updatedAt: LocalDateTime = LocalDateTime.now()
 }
