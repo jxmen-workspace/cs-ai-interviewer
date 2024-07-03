263e7669753648aac3077d61de50c7c28f96ca28
jxmen <sprnd645@gmail.com>
2024-07-04 00:41:05 +0900

    build: spring aop 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
 
     // database
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
