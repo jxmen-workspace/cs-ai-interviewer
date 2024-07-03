afe6f0d732d07c6436f7b126dfac87411ac4f72f
jxmen <sprnd645@gmail.com>
2024-07-04 00:49:13 +0900

    feat: adapter에 수행 시간 기록 적용
    패키지 위치 이동 시 해당 경로명도 수정해야 함

---
+++ src/main/kotlin/dev/jxmen/cs/ai/interviewer/infra/aop/ExecutionTimeLoggingAspect.kt
@@ -0,0 +1,37 @@
+package dev.jxmen.cs.ai.interviewer.infra.aop
+
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Pointcut
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+
+@Aspect
+@Component
+class ExecutionTimeLoggingAspect {
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
+    @Pointcut(
+        "within(dev.jxmen.cs.ai.interviewer.domain..*.service.adapter..*) || within(dev.jxmen.cs.ai.interviewer.external.adapter..*)",
+    )
+    fun adapterMethods() {}
+
+    @Around("adapterMethods()")
+    fun logExecutionTime(proceedingJoinPoint: ProceedingJoinPoint): Any? {
+        val startTime = System.currentTimeMillis()
+        try {
+            return proceedingJoinPoint.proceed()
+        } finally {
+            val endTime = System.currentTimeMillis()
+
+            val simpleClassName =
+                proceedingJoinPoint.signature.declaringTypeName
+                    .split(".")
+                    .last() // 맨 마지막 클래스명만 사용
+            val methodName = proceedingJoinPoint.signature.name
+
+            logger.info("Execution time of $simpleClassName.$methodName: ${endTime - startTime} ms")
+        }
+    }
+}
