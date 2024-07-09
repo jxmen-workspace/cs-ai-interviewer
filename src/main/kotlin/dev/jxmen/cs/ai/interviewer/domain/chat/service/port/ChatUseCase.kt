67c52b01615d94c3f9dc971dc37d73e831f55408
jxmen <sprnd645@gmail.com>
2024-07-09 23:33:55 +0900

    fix: 채팅 내역이 없을 경우 question 추가 로직 삭제
    ai에게 권한 부여/ai가 해당 권한 부여에 대한 답변은 기존 채팅 목록이 없다면 Claude에서 생성하기 때문에 처음 답변부터 저장하도록 수정한다.

--- src/main/kotlin/dev/jxmen/cs/ai/interviewer/domain/chat/service/port/ChatUseCase.kt
+++ src/main/kotlin/dev/jxmen/cs/ai/interviewer/domain/chat/service/port/ChatUseCase.kt
@@ -1,6 +1,5 @@
 package dev.jxmen.cs.ai.interviewer.domain.chat.service.port
 
-import dev.jxmen.cs.ai.interviewer.domain.chat.Chat
 import dev.jxmen.cs.ai.interviewer.domain.subject.Subject
 
 interface ChatUseCase {
@@ -10,6 +9,5 @@ interface ChatUseCase {
         answer: String,
         nextQuestion: String,
         score: Int,
-        chats: List<Chat>,
     )
 }
