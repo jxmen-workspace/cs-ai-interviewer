5b45466f85b8d8cac87f71726edbb8da8719c038
jxmen <sprnd645@gmail.com>
2024-07-11 16:43:59 +0900

    refactor: JpaRepository infrastructure/persistence 패키지로 이동

--- src/main/kotlin/dev/jxmen/cs/ai/interviewer/infrastructure/persistence/JpaChatCommandRepository.kt
+++ src/main/kotlin/dev/jxmen/cs/ai/interviewer/infrastructure/persistence/JpaChatCommandRepository.kt
@@ -1,4 +1,4 @@
-package dev.jxmen.cs.ai.interviewer.infrastructure
+package dev.jxmen.cs.ai.interviewer.infrastructure.persistence
 
 import dev.jxmen.cs.ai.interviewer.domain.chat.Chat
 import dev.jxmen.cs.ai.interviewer.domain.chat.ChatCommandRepository
