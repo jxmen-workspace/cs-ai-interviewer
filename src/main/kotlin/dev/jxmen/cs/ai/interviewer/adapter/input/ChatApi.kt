fab08542c6c76260dc2a809edbfce3475e269be7
jxmen <sprnd645@gmail.com>
2024-07-13 23:42:23 +0900

    feat: 채팅 목록 조회 API V2

--- src/main/kotlin/dev/jxmen/cs/ai/interviewer/adapter/input/ChatApi.kt
+++ src/main/kotlin/dev/jxmen/cs/ai/interviewer/adapter/input/ChatApi.kt
@@ -3,6 +3,7 @@ package dev.jxmen.cs.ai.interviewer.adapter.input
 import dev.jxmen.cs.ai.interviewer.adapter.input.dto.response.ChatMessageResponse
 import dev.jxmen.cs.ai.interviewer.application.port.input.ChatQuery
 import dev.jxmen.cs.ai.interviewer.application.port.input.SubjectQuery
+import dev.jxmen.cs.ai.interviewer.domain.member.Member
 import dev.jxmen.cs.ai.interviewer.global.dto.ListDataResponse
 import jakarta.servlet.http.HttpSession
 import org.springframework.data.repository.query.Param
@@ -18,6 +19,8 @@ class ChatApi(
     private val subjectQuery: SubjectQuery,
     private val chatQuery: ChatQuery,
 ) {
+
+    @Deprecated("Use getMessagesV2 instead")
     @GetMapping("/messages")
     fun getMessages(
         @Param("subjectId") subjectId: String,
@@ -29,6 +32,32 @@ class ChatApi(
             chatQuery.findBySubjectAndUserSessionId(
                 subject = subject,
                 userSessionId = userSessionId,
+            )
+
+        return ResponseEntity.ok(
+            ListDataResponse(
+                messages.map {
+                    ChatMessageResponse(
+                        message = it.message,
+                        score = it.score,
+                        type = it.chatType,
+                    )
+                },
+            ),
+        )
+    }
+
+    @GetMapping("/messages", headers = ["X-Api-Version=2"])
+    fun getMessagesV2(
+        @Param("subjectId") subjectId: String,
+    ): ResponseEntity<ListDataResponse<ChatMessageResponse>> {
+        val member = httpSession.getAttribute("member") as Member
+        val subject = subjectQuery.findById(subjectId.toLong())
+
+        val messages =
+            chatQuery.findBySubjectAndMember(
+                subject = subject,
+                member = member
             )
 
         return ResponseEntity.ok(
