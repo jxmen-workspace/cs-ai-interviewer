6f698da1f1f3b8fcbd89659eae43f88b0eaf7828
jxmen <sprnd645@gmail.com>
2024-07-15 23:51:35 +0900

    fix: CSRF repository 및 핸들러 추가
    공식 문서에서는 HttpOnly를 비활성화 했지만 내 환경에서는 http를 사용할 것이므로 `withHttpOnlyFalse`를 활성화하지 않는다.
    
    https://docs.spring.io/spring-security/reference/servlet/exploits/csrf.html#csrf-integration-javascript
    
    Close #68

--- src/main/kotlin/dev/jxmen/cs/ai/interviewer/global/config/SecurityConfig.kt
+++ src/main/kotlin/dev/jxmen/cs/ai/interviewer/global/config/SecurityConfig.kt
@@ -3,6 +3,10 @@ package dev.jxmen.cs.ai.interviewer.global.config
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import dev.jxmen.cs.ai.interviewer.global.config.service.CustomOAuth2UserService
 import dev.jxmen.cs.ai.interviewer.global.dto.ErrorResponse
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletException
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,6 +16,16 @@ import org.springframework.http.MediaType
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository
+import org.springframework.security.web.csrf.CsrfToken
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler
+import org.springframework.security.web.csrf.CsrfTokenRequestHandler
+import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler
+import org.springframework.util.StringUtils
+import org.springframework.web.filter.OncePerRequestFilter
+import java.io.IOException
+import java.util.function.Supplier
 
 @Suppress("ktlint:standard:chain-method-continuation") // NOTE: 활성화시 오히려 가독성이 저하되어 비활성화
 @EnableWebSecurity
@@ -29,14 +43,16 @@ class SecurityConfig(
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         if (activeProfile == "local" || activeProfile == "default") {
             // set h2 console to be accessible
-            http
-                .csrf { it.disable() }
-                .headers {
-                    it.frameOptions { it.disable() }
-                }
+            http.headers {
+                it.frameOptions { it.disable() }
+            }
         }
 
         http
+            .csrf {
+                    it.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    it.csrfTokenRequestHandler(SpaCsrfTokenRequestHandler())
+            }
             .authorizeHttpRequests {
                 it.requestMatchers("/h2-console/**").permitAll()
                     .requestMatchers("/").permitAll()
@@ -50,12 +66,12 @@ class SecurityConfig(
                     .requestMatchers(HttpMethod.POST, "/api/v2/subjects/{subjectId}/answer").authenticated()
                     .requestMatchers(HttpMethod.GET, "/api/v2/chat/messages").authenticated()
                     .anyRequest().authenticated()
-            }.oauth2Login {
-                it.userInfoEndpoint {
-                    it.userService(customOAuth2UserService)
-                }
-            }.exceptionHandling {
-                it.authenticationEntryPoint { request, response, authException ->
+            }
+            .oauth2Login {
+                it.userInfoEndpoint { it.userService(customOAuth2UserService) }
+            }
+            .exceptionHandling {
+                it.authenticationEntryPoint { _, response, authException ->
                     // 인증되지 않거나 실패할 경우 공개된 API 외 401 응답
                     response.status = HttpStatus.UNAUTHORIZED.value()
                     response.setHeader("Content-Type", MediaType.APPLICATION_JSON.type)
@@ -70,8 +86,53 @@ class SecurityConfig(
                 }
             }
 
+        http.addFilterAfter(CsrfCookieFilter(), BasicAuthenticationFilter::class.java)
+
         return http.build()
     }
 
     private fun toJson(obj: Any): String = objectMapper.writeValueAsString(obj)
+}
+
+class SpaCsrfTokenRequestHandler : CsrfTokenRequestAttributeHandler() {
+    private val delegate: CsrfTokenRequestHandler = XorCsrfTokenRequestAttributeHandler()
+
+    override fun handle(request: HttpServletRequest, response: HttpServletResponse, csrfToken: Supplier<CsrfToken>) {
+        /*
+         * Always use XorCsrfTokenRequestAttributeHandler to provide BREACH protection of
+         * the CsrfToken when it is rendered in the response body.
+         */
+        delegate.handle(request, response, csrfToken)
+    }
+
+    override fun resolveCsrfTokenValue(request: HttpServletRequest, csrfToken: CsrfToken): String? {
+        /*
+         * If the request contains a request header, use CsrfTokenRequestAttributeHandler
+         * to resolve the CsrfToken. This applies when a single-page application includes
+         * the header value automatically, which was obtained via a cookie containing the
+         * raw CsrfToken.
+         */
+        return if (StringUtils.hasText(request.getHeader(csrfToken.headerName))) {
+            super.resolveCsrfTokenValue(request, csrfToken)
+        } else {
+            /*
+             * In all other cases (e.g. if the request contains a request parameter), use
+             * XorCsrfTokenRequestAttributeHandler to resolve the CsrfToken. This applies
+             * when a server-side rendered form includes the _csrf request parameter as a
+             * hidden input.
+             */
+            delegate.resolveCsrfTokenValue(request, csrfToken)
+        }
+    }
+}
+
+class CsrfCookieFilter : OncePerRequestFilter() {
+
+    @Throws(ServletException::class, IOException::class)
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+        val csrfToken = request.getAttribute("_csrf") as CsrfToken
+        // Render the token value to a cookie by causing the deferred token to be loaded
+        csrfToken.token
+        filterChain.doFilter(request, response)
+    }
 }
