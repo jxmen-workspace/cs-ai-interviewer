3c93bae850a7596f915e63ac4710e74799ebe444
jxmen <sprnd645@gmail.com>
2024-08-14 01:03:03 +0900

    feat: JpaChatArchive 엔티티 추가

---
+++ src/main/kotlin/dev/jxmen/cs/ai/interviewer/adapter/output/persistence/JpaChatArchive.kt
@@ -0,0 +1,37 @@
+package dev.jxmen.cs.ai.interviewer.adapter.output.persistence
+
+import dev.jxmen.cs.ai.interviewer.domain.BaseEntity
+import dev.jxmen.cs.ai.interviewer.domain.member.Member
+import dev.jxmen.cs.ai.interviewer.domain.subject.Subject
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.Index
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+
+/**
+ * 멤버는 여러개의 채팅 아카이브를 가진다. (1:N)
+ * 채팅 아카이브는 하나의 주제에 속한다. (N:1)
+ */
+@Entity
+@Suppress("ktlint:standard:no-blank-line-in-list")
+@Table(
+    name = "chat_archive",
+    indexes = [
+        Index(
+            name = "chat_archive_member_id_subject_id_idx",
+            columnList = "member_id, subject_id",
+        ),
+    ],
+)
+class JpaChatArchive(
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subject_id")
+    val subject: Subject,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    val member: Member,
+) : BaseEntity()
---
+++ src/main/kotlin/dev/jxmen/cs/ai/interviewer/adapter/output/persistence/JpaChatArchiveContent.kt
@@ -0,0 +1,34 @@
+package dev.jxmen.cs.ai.interviewer.adapter.output.persistence
+
+import dev.jxmen.cs.ai.interviewer.domain.BaseEntity
+import dev.jxmen.cs.ai.interviewer.domain.chat.ChatContent
+import jakarta.persistence.Embedded
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.Index
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import org.hibernate.annotations.Comment
+
+@Entity
+@Table(
+    name = "chat_archive_content",
+    indexes = [
+        Index(
+            name = "chat_archive_content_chat_archive_id_idx",
+            columnList = "chat_archive_id",
+        ),
+    ],
+)
+@Suppress("ktlint:standard:no-blank-line-in-list")
+class JpaChatArchiveContent(
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_archive_id")
+    @Comment("채팅 아카이브 아이디")
+    val archive: JpaChatArchive,
+
+    @Embedded
+    val content: ChatContent,
+) : BaseEntity()
