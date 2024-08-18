d1c3cdb6cb038323d7e40304057c7d63026ab0df
jxmen <sprnd645@gmail.com>
2024-08-18 22:44:57 +0900

    refactor: 미사용 API 제거

---
+++ src/main/kotlin/dev/jxmen/cs/ai/interviewer/global/config/security/MethodSecurityConfig.kt
@@ -0,0 +1,8 @@
+package dev.jxmen.cs.ai.interviewer.global.config.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true)
+class MethodSecurityConfig
