137178a03d706d815888018efb409e4dbf6fdeb2
jxmen <sprnd645@gmail.com>
2024-08-26 04:20:23 +0900

    refactor: 멤버 채팅 시나리오 검증 메서드로 분리

--- src/test/kotlin/dev/jxmen/cs/ai/interviewer/MemberScenarioTest.kt
+++ src/test/kotlin/dev/jxmen/cs/ai/interviewer/MemberScenarioTest.kt
@@ -147,93 +147,22 @@ class MemberScenarioTest(
                 }
 
                 it("멤버가 채팅을 하면 채팅이 저장된다") {
-                    mockMvc
-                        .get("/api/v1/subjects/my")
-                        .andExpect {
-                            status { isOk() }
-                            jsonPath("$.success") { value(true) }
-                            jsonPath("$.data") { haveLength(1) }
-                            jsonPath("$.data[0].id") { value(subject.id) }
-                            jsonPath("$.data[0].title") { value(subject.title) }
-                            jsonPath("$.data[0].category") { value(subject.category.name) }
-                            jsonPath("$.data[0].maxScore") { value(null) }
-                            jsonPath("$.error") { value(null) }
-                        }
+                    validateMySubjectHasValues(subject)
+                    validateChatIsEmpty(subject)
 
-                    mockMvc
-                        .get("/api/v1/subjects/${subject.id}/chats")
-                        .andExpect {
-                            status { isOk() }
-                            jsonPath("$.success") { value(true) }
-                            jsonPath("$.data") { isEmpty() }
-                            jsonPath("$.error") { isEmpty() }
-                        }
+                    answer(subject, answer)
 
-                    webTestClient
-                        .get()
-                        .uri("/api/v5/subjects/{subjectId}/answer?message={message}", subject.id, answer)
-                        .exchange()
-                        .expectStatus()
-                        .isOk
-
-                    val now = LocalDateTime.now()
-                    mockMvc
-                        .get("/api/v1/subjects/${subject.id}/chats")
-                        .andExpect {
-                            status { isOk() }
-                            jsonPath("$.data") { haveLength(3) }
-                            jsonPath("$.data[0].type") { value("question") }
-                            jsonPath("$.data[0].message") { value(subject.question) }
-                            jsonPath("$.data[0].score") { value(null) }
-                            jsonPath("$.data[0].createdAt") { value(null) }
-                            jsonPath("$.data[1].type") { value("answer") }
-                            jsonPath("$.data[1].message") { value(answer) }
-                            jsonPath("$.data[1].score") { value(10) }
-                            jsonPath("$.data[1].createdAt") { value(matcher = BeforeDateMatcher(now)) }
-                            jsonPath("$.data[2].type") { value("question") }
-                            jsonPath("$.data[2].message") { value(nextQuestion) }
-                            jsonPath("$.data[2].score") { value(null) }
-                            jsonPath("$.data[2].createdAt") { value(null) }
-                        }
-
-                    mockMvc
-                        .get("/api/v1/subjects/my")
-                        .andExpect {
-                            status { isOk() }
-                            jsonPath("$.data[0].maxScore") { value(10) }
-                        }
+                    validateChatSaved(subject, answer, nextQuestion)
+                    validateMySubjectHasMaxScore(10)
                 }
 
                 it("멤버가 채팅을 초기화(아카이브)하면 채팅 내역이 지워지고, 아카이브에 저장된다") {
-                    webTestClient
-                        .get()
-                        .uri("/api/v5/subjects/{subjectId}/answer?message={message}", subject.id, "test answer")
-                        .exchange()
-                        .expectStatus()
-                        .isOk
+                    answer(subject, "test answer")
 
-                    mockMvc
-                        .post("/api/v2/subjects/${subject.id}/chats/archive")
-                        .andExpect {
-                            status { isCreated() }
-                            jsonPath("$.success") { value(true) }
-                            jsonPath("$.data") { value(null) }
-                            jsonPath("$.error") { value(null) }
-                        }
+                    archive(subject)
 
-                    mockMvc
-                        .get("/api/v1/subjects/${subject.id}/chats")
-                        .andExpect {
-                            status { isOk() }
-                            jsonPath("$.data") { isEmpty() }
-                        }
-
-                    mockMvc
-                        .get("/api/v1/subjects/my")
-                        .andExpect {
-                            status { isOk() }
-                            jsonPath("$.data[0].maxScore") { value(null) }
-                        }
+                    validateChatIsEmpty(subject)
+                    validateMySubjectHasNoMaxScore()
 
                     // NOTE: 추후 API 개발 시 아카이브 내용을 확인하는 API를 추가해야 한다.
                     val archives = chatArchiveQueryRepository.findBySubjectAndMember(subject, member)
@@ -244,6 +173,99 @@ class MemberScenarioTest(
                 }
             }
         }
+    }
+
+    private fun validateChatSaved(
+        subject: Subject,
+        answer: String,
+        nextQuestion: String,
+    ) {
+        val now = LocalDateTime.now()
+        mockMvc
+            .get("/api/v1/subjects/${subject.id}/chats")
+            .andExpect {
+                status { isOk() }
+                jsonPath("$.data") { haveLength(3) }
+                jsonPath("$.data[0].type") { value("question") }
+                jsonPath("$.data[0].message") { value(subject.question) }
+                jsonPath("$.data[0].score") { value(null) }
+                jsonPath("$.data[0].createdAt") { value(null) }
+                jsonPath("$.data[1].type") { value("answer") }
+                jsonPath("$.data[1].message") { value(answer) }
+                jsonPath("$.data[1].score") { value(10) }
+                jsonPath("$.data[1].createdAt") { value(matcher = BeforeDateMatcher(now)) }
+                jsonPath("$.data[2].type") { value("question") }
+                jsonPath("$.data[2].message") { value(nextQuestion) }
+                jsonPath("$.data[2].score") { value(null) }
+                jsonPath("$.data[2].createdAt") { value(null) }
+            }
+    }
+
+    private fun validateMySubjectHasMaxScore(maxScore: Int) {
+        mockMvc
+            .get("/api/v1/subjects/my")
+            .andExpect {
+                status { isOk() }
+                jsonPath("$.data[0].maxScore") { value(maxScore) }
+            }
+    }
+
+    private fun validateMySubjectHasValues(subject: Subject) {
+        mockMvc
+            .get("/api/v1/subjects/my")
+            .andExpect {
+                status { isOk() }
+                jsonPath("$.success") { value(true) }
+                jsonPath("$.data") { haveLength(1) }
+                jsonPath("$.data[0].id") { value(subject.id) }
+                jsonPath("$.data[0].title") { value(subject.title) }
+                jsonPath("$.data[0].category") { value(subject.category.name) }
+                jsonPath("$.data[0].maxScore") { value(null) }
+                jsonPath("$.error") { value(null) }
+            }
+    }
+
+    private fun validateMySubjectHasNoMaxScore() {
+        mockMvc
+            .get("/api/v1/subjects/my")
+            .andExpect {
+                status { isOk() }
+                jsonPath("$.data[0].maxScore") { value(null) }
+            }
+    }
+
+    private fun archive(subject: Subject) {
+        mockMvc
+            .post("/api/v2/subjects/${subject.id}/chats/archive")
+            .andExpect {
+                status { isCreated() }
+                jsonPath("$.success") { value(true) }
+                jsonPath("$.data") { value(null) }
+                jsonPath("$.error") { value(null) }
+            }
+    }
+
+    private fun answer(
+        subject: Subject,
+        answer: String,
+    ) {
+        webTestClient
+            .get()
+            .uri("/api/v5/subjects/{subjectId}/answer?message={message}", subject.id, answer)
+            .exchange()
+            .expectStatus()
+            .isOk
+    }
+
+    private fun validateChatIsEmpty(subject: Subject) {
+        mockMvc
+            .get("/api/v1/subjects/${subject.id}/chats")
+            .andExpect {
+                status { isOk() }
+                jsonPath("$.success") { value(true) }
+                jsonPath("$.data") { isEmpty() }
+                jsonPath("$.error") { isEmpty() }
+            }
     }
 
     private fun setAuthentication(member: Member) {
