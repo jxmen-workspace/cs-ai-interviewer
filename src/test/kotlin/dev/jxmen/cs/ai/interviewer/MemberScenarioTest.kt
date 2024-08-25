1e793b56384ee7264fad9c33bc9c3b1ccffa017d
jxmen <sprnd645@gmail.com>
2024-08-26 03:18:05 +0900

    refactor: 시나리오 테스트 kotest로 변경

--- src/test/kotlin/dev/jxmen/cs/ai/interviewer/MemberScenarioTest.kt
+++ src/test/kotlin/dev/jxmen/cs/ai/interviewer/MemberScenarioTest.kt
@@ -10,12 +10,12 @@ import dev.jxmen.cs.ai.interviewer.persistence.port.output.ChatArchiveContentQue
 import dev.jxmen.cs.ai.interviewer.persistence.port.output.ChatArchiveQueryRepository
 import dev.jxmen.cs.ai.interviewer.persistence.port.output.MemberCommandRepository
 import dev.jxmen.cs.ai.interviewer.persistence.port.output.SubjectCommandRepository
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.extensions.spring.SpringExtension
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.haveLength
 import org.hamcrest.BaseMatcher
 import org.hamcrest.Description
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
 import org.mockito.kotlin.given
 import org.mockito.kotlin.willReturn
@@ -50,232 +50,201 @@ class MemberScenarioTest(
     private val chatArchiveQueryRepository: ChatArchiveQueryRepository,
     private val chatArchiveContentQueryRepository: ChatArchiveContentQueryRepository,
     private val chatAppender: ChatAppender,
-) {
+) : DescribeSpec() {
+    override fun extensions() = listOf(SpringExtension)
+
+    private lateinit var mockMvc: MockMvc
+    private lateinit var webTestClient: WebTestClient
+
     @MockBean
     lateinit var reactiveMemberChatService: ReactiveMemberChatUseCase
 
-    lateinit var mockMvc: MockMvc
-    lateinit var webTestClient: WebTestClient
-
-    @BeforeEach
-    fun setUp() {
-        // kotlin에서는 버그로 인해 필터 추가 불가 - https://docs.spring.io/spring-framework/reference/testing/spring-mvc-test-framework/server-filters.html
-        mockMvc = MockMvcBuilders.webAppContextSetup(context).build()
-
-        /**
-         * WebTestClient는 WebFlux를 사용하는 경우에 사용. mockMvc는 MockMvcWebTestClient를 사용
-         */
-        webTestClient = MockMvcWebTestClient.bindToApplicationContext(context).build()
-    }
-
-    // TODO: 시나리오 잘게 쪼개기
-    @Test
-    fun `멤버 답변 및 채팅 내역 시나리오 테스트`() {
-        // 멤버 생성
-        val member = MockMemberArgumentResolver.member
-        memberCommandRepository.save(member)
-
-        // 멤버 인증 정보 저장
-        val oauth2User = createOAuth2User(member)
-        val authentication = createOAuth2AuthenticationToken(oauth2User)
-        SecurityContextHolder.getContext().authentication = authentication
-
-        // 주제 생성
-        val subject =
-            subjectCommandRepository.save(
-                Subject(
-                    title = "test subject",
-                    question = "test question",
-                    category = SubjectCategory.OS,
-                ),
-            )
-
-        // 주제 목록 조회
-        mockMvc
-            .get("/api/v1/subjects") { param("category", "OS") }
-            .andExpect {
-                status { isOk() }
-                jsonPath("$.success") { value(true) }
-                jsonPath("$.data") { isArray() }
-                jsonPath("$.data.length()") { value(1) }
-                jsonPath("$.data[0].id") { value(subject.id) }
-                jsonPath("$.data[0].title") { value("test subject") }
-                jsonPath("$.data[0].category") { value("OS") }
-                jsonPath("$.error") { value(null) }
-            }
-
-        // 주제 상세 조회
-        mockMvc
-            .get("/api/v1/subjects/${subject.id}")
-            .andExpect {
-                status { isOk() }
-                jsonPath("$.success") { value(true) }
-                jsonPath("$.data.id") { value(subject.id) }
-                jsonPath("$.data.title") { value("test subject") }
-                jsonPath("$.data.question") { value("test question") }
-                jsonPath("$.data.category") { value("OS") }
-                jsonPath("$.error") { value(null) }
-            }.andDo {
-                print()
-            }
-
-        // 내 주제 목록 조회
-        mockMvc
-            .get("/api/v1/subjects/my")
-            .andExpect {
-                status { isOk() }
-                jsonPath("$.success") { value(true) }
-                jsonPath("$.data") { haveLength(1) }
-                jsonPath("$.data[0].id") { value(subject.id) }
-                jsonPath("$.data[0].title") { value("test subject") }
-                jsonPath("$.data[0].category") { value("OS") }
-                jsonPath("$.data[0].maxScore") { value(null) }
-                jsonPath("$.error") { value(null) }
-            }
-
-        // 채팅 API 조회 시 빈 값 응답 검증
-        mockMvc
-            .get("/api/v1/subjects/${subject.id}/chats")
-            .andExpect {
-                status { isOk() }
-                jsonPath("$.success") { value(true) }
-                jsonPath("$.data") { isEmpty() }
-                jsonPath("$.error") { isEmpty() }
-            }
-
-        val answer = "test answer"
-        val nextQuestion =
-            """
-            답변에 대한 점수: 10점
-            """.trimIndent().trimMargin()
-        given { reactiveMemberChatService.answerAsync(any()) }.willReturn {
-            Flux
-                .create<ChatResponse?> {
-                    it.next(
-                        ChatResponse(listOf(Generation(answer))),
-                    )
-                    it.complete()
-                }
-                // 차단되지 않는 컨텍스트에서 호출을 차단하면 스레드 고갈이 발생할 수 있습니다.
-                .publishOn(Schedulers.boundedElastic())
-                .doOnComplete {
-                    // NOTE: chatAppender.addAnswerAndNextQuestion() 호출
-                    chatAppender.addAnswerAndNextQuestion(
-                        subject = subject,
-                        member = member,
-                        answer = answer,
-                        chats = emptyList(),
-                        nextQuestion = nextQuestion,
-                    )
-                }
+    init {
+        beforeEach {
+            mockMvc = MockMvcBuilders.webAppContextSetup(context).build()
+            webTestClient = MockMvcWebTestClient.bindToApplicationContext(context).build()
         }
 
-        // 특정 주제에 대해 답변
-        webTestClient
-            .get()
-            .uri("/api/v5/subjects/{subjectId}/answer?message={message}", subject.id, answer)
-            .header("Authorization", "Bearer test-token")
-            .exchange()
-            .expectStatus()
-            .isOk
+        describe("MemberScenarioTest") {
+            it("멤버 답변 및 채팅 내역 시나리오 테스트") {
+                val member = MockMemberArgumentResolver.member
+                memberCommandRepository.save(member)
 
-        // 채팅 API 조회 - 답변과 다음 질문이 생성되었는지 검증
-        val now = LocalDateTime.now()
-        mockMvc
-            .get("/api/v1/subjects/${subject.id}/chats")
-            .andExpect {
-                status { isOk() }
-                jsonPath("$.data") { haveLength(3) }
-                jsonPath("$.data[0].type") { value("question") }
-                jsonPath("$.data[0].message") {
-                    value(
+                val oauth2User = createOAuth2User(member)
+                val authentication = createOAuth2AuthenticationToken(oauth2User)
+                SecurityContextHolder.getContext().authentication = authentication
+
+                val subject =
+                    subjectCommandRepository.save(
                         Subject(
                             title = "test subject",
                             question = "test question",
                             category = SubjectCategory.OS,
-                        ).question,
+                        ),
                     )
+
+                mockMvc
+                    .get("/api/v1/subjects") { param("category", "OS") }
+                    .andExpect {
+                        status { isOk() }
+                        jsonPath("$.success") { value(true) }
+                        jsonPath("$.data") { isArray() }
+                        jsonPath("$.data.length()") { value(1) }
+                        jsonPath("$.data[0].id") { value(subject.id) }
+                        jsonPath("$.data[0].title") { value("test subject") }
+                        jsonPath("$.data[0].category") { value("OS") }
+                        jsonPath("$.error") { value(null) }
+                    }
+
+                mockMvc
+                    .get("/api/v1/subjects/${subject.id}")
+                    .andExpect {
+                        status { isOk() }
+                        jsonPath("$.success") { value(true) }
+                        jsonPath("$.data.id") { value(subject.id) }
+                        jsonPath("$.data.title") { value("test subject") }
+                        jsonPath("$.data.question") { value("test question") }
+                        jsonPath("$.data.category") { value("OS") }
+                        jsonPath("$.error") { value(null) }
+                    }.andDo {
+                        print()
+                    }
+
+                mockMvc
+                    .get("/api/v1/subjects/my")
+                    .andExpect {
+                        status { isOk() }
+                        jsonPath("$.success") { value(true) }
+                        jsonPath("$.data") { haveLength(1) }
+                        jsonPath("$.data[0].id") { value(subject.id) }
+                        jsonPath("$.data[0].title") { value("test subject") }
+                        jsonPath("$.data[0].category") { value("OS") }
+                        jsonPath("$.data[0].maxScore") { value(null) }
+                        jsonPath("$.error") { value(null) }
+                    }
+
+                mockMvc
+                    .get("/api/v1/subjects/${subject.id}/chats")
+                    .andExpect {
+                        status { isOk() }
+                        jsonPath("$.success") { value(true) }
+                        jsonPath("$.data") { isEmpty() }
+                        jsonPath("$.error") { isEmpty() }
+                    }
+
+                val answer = "test answer"
+                val nextQuestion = "답변에 대한 점수: 10점"
+                given { reactiveMemberChatService.answerAsync(any()) }.willReturn {
+                    Flux
+                        .create<ChatResponse?> {
+                            it.next(ChatResponse(listOf(Generation(answer))))
+                            it.complete()
+                        }.publishOn(Schedulers.boundedElastic())
+                        .doOnComplete {
+                            chatAppender.addAnswerAndNextQuestion(
+                                subject = subject,
+                                member = member,
+                                answer = answer,
+                                chats = emptyList(),
+                                nextQuestion = nextQuestion,
+                            )
+                        }
                 }
-                jsonPath("$.data[0].score") { value(null) }
-                jsonPath("$.data[0].createdAt") { value(null) }
-                jsonPath("$.data[1].type") { value("answer") }
-                jsonPath("$.data[1].message") { value(answer) }
-                jsonPath("$.data[1].score") { value(10) }
-                jsonPath("$.data[1].createdAt") { value(matcher = BeforeDateMatcher(now)) }
-                jsonPath("$.data[2].type") { value("question") }
-                jsonPath("$.data[2].message") { value(nextQuestion) }
-                jsonPath("$.data[2].score") { value(null) }
-                jsonPath("$.data[2].createdAt") { value(null) }
+
+                webTestClient
+                    .get()
+                    .uri("/api/v5/subjects/{subjectId}/answer?message={message}", subject.id, answer)
+                    .exchange()
+                    .expectStatus()
+                    .isOk
+
+                val now = LocalDateTime.now()
+                mockMvc
+                    .get("/api/v1/subjects/${subject.id}/chats")
+                    .andExpect {
+                        status { isOk() }
+                        jsonPath("$.data") { haveLength(3) }
+                        jsonPath("$.data[0].type") { value("question") }
+                        jsonPath("$.data[0].message") {
+                            value(
+                                Subject(
+                                    title = "test subject",
+                                    question = "test question",
+                                    category = SubjectCategory.OS,
+                                ).question,
+                            )
+                        }
+                        jsonPath("$.data[0].score") { value(null) }
+                        jsonPath("$.data[0].createdAt") { value(null) }
+                        jsonPath("$.data[1].type") { value("answer") }
+                        jsonPath("$.data[1].message") { value(answer) }
+                        jsonPath("$.data[1].score") { value(10) }
+                        jsonPath("$.data[1].createdAt") { value(matcher = BeforeDateMatcher(now)) }
+                        jsonPath("$.data[2].type") { value("question") }
+                        jsonPath("$.data[2].message") { value(nextQuestion) }
+                        jsonPath("$.data[2].score") { value(null) }
+                        jsonPath("$.data[2].createdAt") { value(null) }
+                    }
+
+                mockMvc
+                    .get("/api/v1/subjects/my")
+                    .andExpect {
+                        status { isOk() }
+                        jsonPath("$.data[0].maxScore") { value(10) }
+                    }
+
+                mockMvc
+                    .post("/api/v2/subjects/${subject.id}/chats/archive")
+                    .andExpect {
+                        status { isCreated() }
+                        jsonPath("$.success") { value(true) }
+                        jsonPath("$.data") { value(null) }
+                        jsonPath("$.error") { value(null) }
+                    }
+
+                mockMvc
+                    .get("/api/v1/subjects/${subject.id}/chats")
+                    .andExpect {
+                        status { isOk() }
+                        jsonPath("$.data") { isEmpty() }
+                    }
+
+                mockMvc
+                    .get("/api/v1/subjects/my")
+                    .andExpect {
+                        status { isOk() }
+                        jsonPath("$.data[0].maxScore") { value(null) }
+                    }
+
+                val archives = chatArchiveQueryRepository.findBySubjectAndMember(subject, member)
+                archives.size shouldBe 1
+
+                val archiveContents = chatArchiveContentQueryRepository.findByArchive(archives[0])
+                archiveContents.size shouldBe 3
             }
-
-        mockMvc
-            .get("/api/v1/subjects/my")
-            .andExpect {
-                status { isOk() }
-                jsonPath("$.data[0].maxScore") { value(10) }
-            }
-
-        // 채팅 아카이브
-        mockMvc
-            .post("/api/v2/subjects/${subject.id}/chats/archive")
-            .andExpect {
-                status { isCreated() }
-
-                // NOTE: location 헤더 값은 알 수 없으므로 검증하지 않는다.
-                jsonPath("$.success") { value(true) }
-                jsonPath("$.data") { value(null) }
-                jsonPath("$.error") { value(null) }
-            }
-
-        // 채팅 내역 재조회 - 아카이브 후 빈 값 응답 검증
-        mockMvc
-            .get("/api/v1/subjects/${subject.id}/chats")
-            .andExpect {
-                status { isOk() }
-                jsonPath("$.data") { isEmpty() }
-            }
-
-        mockMvc
-            .get("/api/v1/subjects/my")
-            .andExpect {
-                status { isOk() }
-                jsonPath("$.data[0].maxScore") { value(null) }
-            }
-
-        // NOTE: 추후 API 개발 완료 시 API 호출로 변경
-        val archives = chatArchiveQueryRepository.findBySubjectAndMember(subject, member)
-        archives.size shouldBe 1
-
-        val archiveContents = chatArchiveContentQueryRepository.findByArchive(archives[0])
-        archiveContents.size shouldBe 3
+        }
     }
 
     private fun createOAuth2AuthenticationToken(
         oauth2User: DefaultOAuth2User,
         provider: String = "google",
-    ): OAuth2AuthenticationToken {
-        val authentication =
-            OAuth2AuthenticationToken(
-                oauth2User,
-                emptyList<GrantedAuthority>(),
-                provider, // NOTE: 구글 외 다른 로그인 수단 추가 시 변경 필요
-            )
-        return authentication
-    }
+    ): OAuth2AuthenticationToken =
+        OAuth2AuthenticationToken(
+            oauth2User,
+            emptyList<GrantedAuthority>(),
+            provider,
+        )
 
-    private fun createOAuth2User(createdMember: Member): DefaultOAuth2User {
-        val oauth2User =
-            DefaultOAuth2User(
-                emptyList<GrantedAuthority>(),
-                mapOf(
-                    "sub" to createdMember.id,
-                    "name" to createdMember.name,
-                    "email" to createdMember.email,
-                ),
-                "sub",
-            )
-        return oauth2User
-    }
+    private fun createOAuth2User(createdMember: Member): DefaultOAuth2User =
+        DefaultOAuth2User(
+            emptyList<GrantedAuthority>(),
+            mapOf(
+                "sub" to createdMember.id,
+                "name" to createdMember.name,
+                "email" to createdMember.email,
+            ),
+            "sub",
+        )
 }
 
 class BeforeDateMatcher(
@@ -285,9 +254,6 @@ class BeforeDateMatcher(
         description?.appendText("date is $date")
     }
 
-    /**
-     * 생성자에서 받은 시간이 현재 시간보다 이전이면 true를 반환한다.
-     */
     override fun matches(actual: Any?): Boolean {
         val now = LocalDateTime.now()
         return now.isAfter(date)
