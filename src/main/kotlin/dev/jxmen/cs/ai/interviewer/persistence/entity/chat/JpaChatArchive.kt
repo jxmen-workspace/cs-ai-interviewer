ec9eabcc7950503bf63fbce9031be98fd2dfd241
jxmen <sprnd645@gmail.com>
2024-08-29 03:32:15 +0900

    fix: JpaChatArchive 삭제했던 생성자 다시 추가
    private 접근제어자 지정 및 supress 추가

--- src/main/kotlin/dev/jxmen/cs/ai/interviewer/persistence/entity/chat/JpaChatArchive.kt
+++ src/main/kotlin/dev/jxmen/cs/ai/interviewer/persistence/entity/chat/JpaChatArchive.kt
@@ -34,4 +34,12 @@ class JpaChatArchive(
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     val member: Member,
-) : BaseEntity()
+) : BaseEntity() {
+    /**
+     * NOTE: Kotlin JDSL에서 사용하기 위해 추가한 생성자
+     */
+    @Suppress("unused")
+    private constructor(id: Long, subject: Subject, member: Member) : this(subject, member) {
+        this.id = id
+    }
+}
