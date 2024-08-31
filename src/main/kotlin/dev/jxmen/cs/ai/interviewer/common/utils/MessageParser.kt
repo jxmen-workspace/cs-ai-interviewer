8401e47c2fb5241f61ab5ea90267b03784a8c244
jxmen <sprnd645@gmail.com>
2024-08-31 18:30:35 +0900

    refactor: cateStr category로 파라미터 변수명 수정

--- src/main/kotlin/dev/jxmen/cs/ai/interviewer/common/utils/MessageParser.kt
+++ src/main/kotlin/dev/jxmen/cs/ai/interviewer/common/utils/MessageParser.kt
@@ -11,10 +11,14 @@ class MessageParser {
         private val SCORE_REGEX = "답변에 대한 점수: (\\d+)점".toRegex()
     }
 
-    fun parseScore(s: String): Int =
-        SCORE_REGEX
-            .find(s)
-            ?.groupValues
-            ?.get(1)
-            ?.toInt() ?: 0
+    fun parseScore(s: String): Int {
+        val parsedScore =
+            SCORE_REGEX
+                .find(s)
+                ?.groupValues
+                ?.get(1)
+                ?.toInt()
+
+        return parsedScore ?: 0 // null이면 0점으로 처리
+    }
 }
